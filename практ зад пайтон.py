94cd9ac10cdef263d6407ff36cf97dd657665f3c
kamilayermekova <90607435+kamilayermekova@users.noreply.github.com>
2021-11-01 12:56:15 +0300

    Add files via upload

---
+++ практ зад пайтон.py
@@ -0,0 +1,21 @@
+print ('Python work!')
+num=5
+print (num)
+print("Comment me :)")
+# print("Comment me :)")
+number = 2
+print(number ** 3)
+getSix = 1467 // 6
+print(getSix)
+string = "Data"
+mult = string * 4
+print(mult)
+number = 6
+if number % 2 == 0:
+ print("Yes")
+i = 4
+while i <= 13:
+ if i != 7 and i != 11:
+    print(i ** 2)
+
+ i += 1
